94f1b756eaa287b07c54b859b4009ee84531e8ad
Sreenath Madathil <sreenathmadathil95@gmail.com>
2020-03-17 12:00:17 +0530

    Angular heroes app

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero :Hero = {
+    id : 1,
+    name : 'Windstorm'
+  };
+  heroes: Hero[];
+  
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+}
